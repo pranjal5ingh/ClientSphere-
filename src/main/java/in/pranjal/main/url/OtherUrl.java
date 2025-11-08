50249f5f0057d3692505281c852fdb3bad738fcf
Pranjal Singh <pranjalsingh77960@gmail.com>
2025-11-09 00:58:03 +0530

    Add files via upload

---
+++ src/main/java/in/pranjal/main/url/OtherUrl.java
@@ -0,0 +1,7 @@
+package in.pranjal.main.url;
+
+public class OtherUrl {
+	
+	public static String IMAGE_UPLOAD_FOLDER = "src/main/resources/static/uploads";
+	public static String IMAGE_UPLOAD_URL = "http://localhost:8080/uploads/";
+}
